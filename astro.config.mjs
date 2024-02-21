896e57e5ec0cfed30ab9677227be1d09cdc62c42
Eduard Valentin <eduard.valentin1996@gmail.com>
2024-02-21 15:38:04 +0200

    added vite image optimizer plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,19 @@ import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 
 import netlify from "@astrojs/netlify/functions";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  integrations: [tailwind(), preact()],
-  renderers: ["@astrojs/renderer-preact"],
-  adapter: netlify()
+	output: "server",
+	integrations: [tailwind(), preact()],
+	renderers: ["@astrojs/renderer-preact"],
+	adapter: netlify(),
+	vite: {
+		plugins: [
+			ViteImageOptimizer({
+				/* pass your config */
+			}),
+		],
+	},
 });
